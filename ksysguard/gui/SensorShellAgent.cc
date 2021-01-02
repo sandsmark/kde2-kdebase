7af49bf1109427a5b7b3c534ad50fb59318328ea
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2021-01-02 17:20:01 +0100

    Revert "update non-colliding ksysguardd2 name"
    This reverts commit aadff8001777de83d10d508c87aa641a665751c5.

--- ksysguard/gui/SensorShellAgent.cc
+++ ksysguard/gui/SensorShellAgent.cc
@@ -75,7 +75,7 @@ SensorShellAgent::start(const QString& host_, const QString& shell_,
 	if (command != "")
 		cmd =  command;
 	else
-		cmd = shell + " " + host + " ksysguardd2";
+		cmd = shell + " " + host + " ksysguardd";
 	*daemon << cmd;
 
 	if (!daemon->start(KProcess::NotifyOnExit, KProcess::All))
